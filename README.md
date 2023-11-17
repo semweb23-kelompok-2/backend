ef62b0a21ca72b2a556eb652825fb507a2a9ee48
David Alexander <david.alexander01@ui.ac.id>
2023-11-17 13:17:27 +0700

    create sample working query

---
+++ README.md
@@ -0,0 +1,13 @@
+# Kukusan Finder Backend
+
+An API backend developed with FAST API.
+
+## Setup
+
+Install dependencies
+
+`pip install -r requirements.txt`
+
+Run the server
+
+`uvicorn main:app --reload`
--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+import rdflib
 
 app = FastAPI()
 
+g = rdflib.Graph()
+g.parse("static/steam.ttl")
 
-@app.get("/")
+
+@app.get("/api")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/api/{app_id}")
+async def read_app(app_id: str):
+    query = f"""
+        SELECT ?app ?app_name
+        WHERE {{
+            ?app <http://127.0.0.1:8000/appid> "{app_id}" .
+            ?app rdfs:label ?app_name .
+        }}
+    """
+
+    res = g.query(query)
+
+    for row in res:
+        print(row)
+    return res
